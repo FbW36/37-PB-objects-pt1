b4479481a8cdd1becee9ac1551422c882f0d7db9
Zarzarius <zarzarius@gmail.com>
2020-11-03 22:12:19 +0100

    exercise almost done

--- Azael/app.js
+++ Azael/app.js
@@ -1,10 +1,28 @@
 // **1. Object Person.**
 // Create an object named person. Loop through the object and print both the property and value of the object.
 
-const person = {};
+const person = {
+  name: "Santo",
+  family_name: "Borrego",
+  email: "pacasa@tomasa.com",
+  adress: {
+    street: "Kölner str.",
+    number: "66",
+    city: "Bonn",
+  },
+};
+
+console.log("object :>> ", Object.keys(person)); //[ 'name', 'family_name', 'email', 'adress' ]
+
+console.log("object :>> ", Object.values(person)); //[
+// 'Santo',
+// 'Borrego',
+// 'pacasa@tomasa.com',
+// { street: 'Kölner str.', number: '66', city: 'Bonn' }
 
 // **2. Get Values.**
 // Create a function that returns an array of all **values** of an object's properties.
+
 // * Examples:
 // ```javascript
 // getObjectValues({
@@ -17,6 +35,15 @@ const person = {};
 // ```javascript
 // ["tea", "coffee", "milk"]
 // ```
+
+const objectToArray = Object.values(person);
+
+console.log("objectToArray :>> ", objectToArray); //[
+//     'Santo',
+//     'Borrego',
+//     'pacasa@tomasa.com',
+//     { street: 'Kölner str.', number: '66', city: 'Bonn' }
+//   ]
 
 // **3. Add A Method.**
 // Create an object and add a method to that object which prints the values of the objects in a string.
@@ -32,6 +59,17 @@ const person = {};
 // ```
 // * Example of Expected Output
 // "Michael Smith is a 20 year old driver in Paris".
+
+const user = {
+  name: "Aza",
+  email: "abcd@gmail.com",
+  tel: 123456789,
+};
+
+console.log(
+  "Values for user :>> ",
+  `${user.name} with mail ${user.email} and telephone ${user.tel}`
+);
 
 // ## Bonus Questions
 
@@ -62,6 +100,19 @@ const person = {};
 // [["cats", 1], ["dogs", 2], ["turtles", 4]]
 // ```
 
+const myCar = {
+  make: "Ford",
+  model: "Mustang",
+  year: 1969,
+};
+
+function obToArray(object) {
+  const obToArray = Object.entries(object);
+  return obToArray;
+}
+
+console.log("obToArray :>> ", obToArray(myCar));
+
 // **2. List Properties.**
 // Create a function that returns an array of **properties** of a javascript object.
 // * Example
@@ -76,7 +127,12 @@ const person = {};
 // ```javascript
 // ["name", "class", "course"]
 // ```
+function obToArray2(object) {
+  const obToArray = Object.values(object);
+  return obToArray;
+}
 
+console.log("obToArray2 :>> ", obToArray2(myCar));
 // **3. Merge.**
 // Create a function that takes two objects as its parameters and merges them together into a new object.
 // * Example
@@ -103,7 +159,11 @@ const person = {};
 // ```javascript
 // {"John": name, "teacher": job}
 // ```
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
 
+const returnedTarget = Object.assign(target, source);
+console.log("returnedTarget :>> ", returnedTarget);
 // **5. Return Keys and Values.**
 // Write a program that takes an object and returns an array which contains two arrays: one for the keys of the object and the other for the values of the object.
 
